ad9e0a074a9ea45842f1999138703c7b01f9e82c
Oscar Ayala <titoayala@gmail.com>
2022-08-05 02:35:49 -0400

    Aplicando estilos requeridos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,19 @@
 import Card from 'react-bootstrap/Card';
 
 function Cards(props) {
+    const estiloTitulo = {fontSize:'1.5rem', color: 'black'}
+    const estiloDescripcion = {fontSize:'1rem', color: 'gray'}
     return (
-        <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={props.src} />
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>{props.descripcion}</Card.Text>
-            </Card.Body>
-        </Card>
+        <div>
+            <Card className='tarjeta' style={{ width: "25rem" }}>
+                <Card.Img variant="top" src={props.src} />
+                <Card.Body>
+                    <Card.Title style={estiloTitulo}>{props.title}</Card.Title>
+                    <Card.Text style={estiloDescripcion}>{props.descripcion}</Card.Text>
+                </Card.Body>
+            </Card>
+            <hr />
+        </div>
     );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import Alert from 'react-bootstrap/Alert';
 const Footer = () => {
     return (
         <div>
-            <hr />
             <Alert key={'success'} variant={'success'}>
                 Desea ver mas fotos gratis? Haga click en {' '}
                 <Alert.Link href="https://www.pexels.com/es-es/" target="_blank">Este enlace.</Alert.Link>
