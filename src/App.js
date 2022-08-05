b6eaa490a1ed201ed4b2877f6ee292a994b42a59
Oscar Ayala <titoayala@gmail.com>
2022-08-05 01:58:15 -0400

    colocando imágenes a la estructura y organizando componente Card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Cards from "./components/Card";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GridExample from "./components/Card";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <GridExample />
+      <Cards src="./imgs/dobleplay.jpg" title="JUGADA 1" descripcion="Jugada de doble play" />
+      <Cards src="./imgs/outentercera.jpg" title="JUGADA 2" descripcion="Out esperado en tercera base" />
+      <Cards src="./imgs/outrobando.jpg" title="JUGADA 3" descripcion="Out robando base" />
+      <Cards src="./imgs/pitcher.jpg" title="POSICION" descripcion="El pitcher es la posición N°1" />
+      <Cards src="./imgs/quietoenhome.jpg" title="ARBITRO" descripcion="Es el juez del juego" />
+      <Cards src="./imgs/reunion.jpg" title="MANAGER" descripcion="Reune al equipo para dar indicaciones" />
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
+import Alert from 'react-bootstrap/Alert';
+
 const Footer = () => {
     return (
         <div>
             <hr />
-            <h1>Todos los derechos reservados</h1>
+            <Alert key={'success'} variant={'success'}>
+                Desea ver mas fotos gratis? Haga click en {' '}
+                <Alert.Link href="https://www.pexels.com/es-es/" target="_blank">Este enlace.</Alert.Link>
+            </Alert>
         </div>
     );
 };
